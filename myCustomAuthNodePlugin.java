78bb0089ba6cdec0a39aeeb12f0daa5cb5d182fe
Mrikhi06 <81534120+Mrikhi06@users.noreply.github.com>
2021-04-24 23:04:23 +0530

    Update myCustomAuthNodePlugin.java

--- myCustomAuthNodePlugin.java
+++ myCustomAuthNodePlugin.java
@@ -1,18 +1,3 @@
-/*
- * The contents of this file are subject to the terms of the Common Development and
- * Distribution License (the License). You may not use this file except in compliance with the
- * License.
- *
- * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
- * specific language governing permission and limitations under the License.
- *
- * When distributing Covered Software, include this CDDL Header Notice in each file and include
- * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
- * Header, with the fields enclosed by brackets [] replaced by your own identifying
- * information: "Portions copyright [year] [name of copyright owner]".
- *
- * Copyright 2017-2018 ForgeRock AS.
- */
 
 package com.example.customAuthNode;
 
@@ -29,37 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-/**
- * Definition of an <a href="https://backstage.forgerock.com/docs/am/6/apidocs/org/forgerock/openam/auth/node/api/AbstractNodeAmPlugin.html">AbstractNodeAmPlugin</a>. 
- * Implementations can use {@code @Inject} setters to get access to APIs 
- * available via Guice dependency injection. For example, if you want to add an SMS service on install, you 
- * can add the following setter:
- * <pre><code>
- * {@code @Inject}
- * public void setPluginTools(PluginTools tools) {
- *     this.tools = tools;
- * }
- * </code></pre>
- * So that you can use the addSmsService api to load your schema XML for example.
- * PluginTools javadoc may be found 
- * <a href="https://backstage.forgerock.com/docs/am/6/apidocs/org/forgerock/openam/plugins/PluginTools.html#addSmsService-java.io.InputStream-">here</a> 
- * <p>
- *     It can be assumed that when running, implementations of this class will be singleton instances.
- * </p>
- * <p>
- *     It should <i>not</i> be expected that the runtime singleton instances will be the instances on which
- *     {@link #onAmUpgrade(String, String)} will be called. Guice-injected properties will also <i>not</i> be populated
- *     during that method call.
- * </p>
- * <p>
- *     Plugins should <i>not</i> use the {@code ShutdownManager}/{@code ShutdownListener} API for handling shutdown, as
- *     the order of calling those listeners is not deterministic. The {@link #onShutdown()} method for all plugins will
- *     be called in the reverse order from the order that {@link #onStartup()} was called, with dependent plugins being
- *     notified after their dependencies for startup, and before them for shutdown.
- * </p>
- * @supported.all.api
- * @since AM 5.5.0
- */
 public class myCustomAuthNodePlugin extends AbstractNodeAmPlugin {
 
 	static private String currentVersion = "1.0.0";
